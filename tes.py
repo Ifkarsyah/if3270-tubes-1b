32e7786ca27f8fd930217d6c7348e0ee751e22ae
Juniardi Akbar <juniardiakbar@gmail.com>
2020-02-13 14:51:05 +0700

    Fix post-pruning

--- tes.py
+++ tes.py
@@ -13,26 +13,26 @@ class Node:
     def __init__(self):
         self.value = ''
         self.children = {}
-        self.children_label = {}
+        self.children_label= {}
         self.label = 'Node'
 
 def print_tree(root, depth=0, indent=4, requirement=None):
     if requirement is None:
-        print("{}{}".format(" " * (indent * depth), root.value))
-    
-    else:    
+        print("{}{}".format(" "*(indent*depth), root.value))
+    else:
+        
         if root.label is 'Node':
-            str_format = "{} == {} --> ({} ?)"
-            print(str_format.format(" " * (indent * depth), requirement, root.value))
+            str_format = "{} == {} / {} / {} --> ({} ?)"
+            print(str_format.format(" " * (indent*depth), requirement, root.children_label, root.label, root.value))
 
         else:
-            str_format = "{} == {} --> {}"
+            str_format = "{} == {} / {} / {} --> {}"
             if root.label:
-                print(str_format.format(" " * (indent * depth), requirement, positive_value))
+                print(str_format.format(" " * (indent*depth), requirement, root.children_label, root.label, positive_value))
             else:
-                print(str_format.format(" " * (indent * depth), requirement, negative_value))
+                print(str_format.format(" " * (indent*depth), requirement, root.children_label, root.label, negative_value))
 
-    if root.children is not None and root.label is 'Node':
+    if root.children is not None:
         for req_path, child_node in root.children.items():
             print_tree(child_node, depth=depth+1, requirement=req_path)
 
@@ -68,14 +68,14 @@ def information_gain(data, target_attribute, attribute):
 
 def id3_build_tree(examples, target_attribute, attributes):
     root = Node()
-    children_label = {}
 
+    children_label = {}
     if (examples[target_attribute] == positive_value).all():
         root.label = True
         children_label[positive_value] = len(examples)
         children_label[negative_value] = 0
         root.children_label = children_label
-        
+
         return root
     
     if (examples[target_attribute] == negative_value).all():
@@ -88,7 +88,6 @@ def id3_build_tree(examples, target_attribute, attributes):
     
     if not attributes:
         root.label = examples[target_attribute].mode()[0] == positive_value
-        
         return root
         
     ig = []
@@ -102,7 +101,7 @@ def id3_build_tree(examples, target_attribute, attributes):
 
     children_label[positive_value] = 0
     children_label[negative_value] = 0 
-
+    
     for vi in values:
         examples_vi = examples[examples[A] == vi]
         
@@ -117,7 +116,7 @@ def id3_build_tree(examples, target_attribute, attributes):
                     next_attr.append(attr)
 
             new_node = id3_build_tree(examples_vi, target_attribute, next_attr)
-
+        
             children_label[positive_value] += new_node.children_label[positive_value]
             children_label[negative_value] += new_node.children_label[negative_value]
 
@@ -142,18 +141,33 @@ def id3_classify(root, example):
         return negative_value
 
 
-def id3_correctness(root, example_test, target_attribute):
-    test_size = len(example_test)
-    correct = 0
+# def id3_correctness(root, example_test, target_attribute):
+#     test_size = len(example_test)
+#     correct = 0
     
-    for i, _ in example_test.iterrows():
-        real_result = example_test.loc[i, target_attribute]
-        id3_result = id3_classify(root, example_test.loc[i, :])
+#     for i, _ in example_test.iterrows():
+#         real_result = example_test.loc[i, target_attribute]
+#         print(example_test.loc[i, :])
+#         print('+++++++++++++++++++++')
+#         id3_result = id3_classify(root, example_test.loc[i, :])
         
-        if real_result == id3_result:
-            correct += 1
+#         # print(real_result, id3_result)
 
-    return correct / test_size
+#         if real_result == id3_result:
+#             correct += 1
+#     return correct/test_size
+
+def id3_error_rate(root):
+    temp_min = 1E10
+    temp_sum = 0
+    
+    for label in root.children_label:
+        if (root.children_label[label] < temp_min):
+            temp_min = root.children_label[label]
+        
+        temp_sum += root.children_label[label]
+
+    return temp_min / temp_sum, temp_sum
 
 def id3_prune(examples, target_attribute, root, tree):
     if root.label != 'Node':
@@ -162,22 +176,18 @@ def id3_prune(examples, target_attribute, root, tree):
     for val in root.children.keys():
         root.children[val] = id3_prune(examples, target_attribute, root.children[val], tree)
     
-    old_correctness = id3_correctness(tree, examples, target_attribute)
-    
-    temp_max = 0
-    most_target = ''
-    
-    for label in root.children_label:
-        if (root.children_label[label] > temp_max):
-            temp_max = root.children_label[label]
-            most_target = label
-        
-    root.label = root.children_label[most_target]
-    new_correctness = id3_correctness(tree, examples, target_attribute)
+    error_rate_sub_tree, sum_sub_tree = id3_error_rate(root)
 
-    if new_correctness > old_correctness:
-        return root
+    # root.label = examples[target_attribute].mode()[0] == positive_value
     
+    total_error_rate_node = 0
+    for val in root.children.keys(): 
+        error_rate_node, sum_node = id3_error_rate(root.children[val])
+        total_error_rate_node += sum_node / sum_sub_tree * error_rate_node
+
+    print(root.value, total_error_rate_node, error_rate_sub_tree)
+    if total_error_rate_node < error_rate_sub_tree:
+        return root
     else:
         root.label = 'Node'
         return root
